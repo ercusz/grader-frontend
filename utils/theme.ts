7107c8e18f0ddbd87a0fee5b58484197e64ed89a
ercusz <eurokungz.xkz@gmail.com>
2022-08-13 08:53:21 +0700

    feat: :sparkles: update theme color palette and add new font

--- utils/theme.ts
+++ utils/theme.ts
@@ -1,12 +1,17 @@
 import { red } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
+import "@fontsource/inter"
+import "@fontsource/inter/300.css"
+import "@fontsource/inter/500.css"
+import "@fontsource/inter/600.css"
+import "@fontsource/inter/800.css"
 
 // Create a light theme instance.
 export const lightTheme = createTheme({
   palette: {
     mode: 'light',
     primary: {
-      main: '#22272E',
+      main: '#222222',
     },
     error: {
       main: red.A400,
@@ -14,6 +19,25 @@ export const lightTheme = createTheme({
     background: {
       default: '#FCFCFC',
       paper: '#FFFFFF',
+    },
+  },
+  typography: {
+    fontFamily: `"Inter", "Helvetica", sans-serif`,
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 800,
+  },
+  components: {
+    MuiTooltip: {
+      styleOverrides: {
+        tooltip: {
+          fontSize: '1rem',
+          color: '#FCFCFC',
+          backgroundColor: '#222222',
+        },
+      },
     },
   },
 });
@@ -29,8 +53,27 @@ export const darkTheme = createTheme({
       main: red.A400,
     },
     background: {
-      default: '#22272E',
-      paper: '#2D333B',
+      default: '#222222',
+      paper: '#18191A',
+    },
+  },
+  typography: {
+    fontFamily: `"Inter", "Helvetica", sans-serif`,
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 800,
+  },
+  components: {
+    MuiTooltip: {
+      styleOverrides: {
+        tooltip: {
+          fontSize: '1rem',
+          color: '#222222',
+          backgroundColor: '#FCFCFC',
+        },
+      },
     },
   },
 });
