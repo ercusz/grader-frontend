2d29c7206c07d5aa5d17769e82ab7cfd9d6d9b9c
ercusz <57826174+ercusz@users.noreply.github.com>
2022-09-23 18:02:57 +0700

    fix: :bug: add missing property in OutputBox.mocks.ts

--- components/output-box/OutputBox.mocks.ts
+++ components/output-box/OutputBox.mocks.ts
@@ -3,6 +3,7 @@ import { IOutputBox } from './OutputBox';
 const base: IOutputBox = {
   language: 'plaintext',
   template: 'Hello World! \nMy name is John Doe😎',
+  queryKey: ["output"]
 };
 
 export const mockOutputBoxProps = {
