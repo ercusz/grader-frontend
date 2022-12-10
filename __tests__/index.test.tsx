b75bcad9f6a63fe9c1e814932bdd1d4fdd71d0c0
ercusz <57826174+ercusz@users.noreply.github.com>
2022-12-10 08:30:29 +0700

    build: :heavy_plus_sign: add jest and rtl, update dependencies

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import Home from '../pages';
+
+import { useSession } from 'next-auth/react';
+
+jest.mock('next-auth/react');
+
+const mockUseSession = useSession as jest.Mock;
+
+describe('Home', () => {
+  it('should render home (unauthenticated)', () => {
+    mockUseSession.mockReturnValue({
+      status: 'unauthenticated',
+      data: null,
+    });
+
+    render(<Home />);
+    expect(screen.getByRole('heading', { name: /grade้r/ }));
+    expect(screen.getByText('“เกรดเด้อ”'));
+    expect.not.stringContaining('คลาสเรียนของฉัน');
+    expect.not.stringContaining('เพลย์กราวด์');
+  });
+
+  it('should render home (authenticated)', () => {
+    mockUseSession.mockReturnValue({
+      status: 'authenticated',
+      data: { username: 'admin' },
+    });
+
+    render(<Home />);
+    expect(screen.getByRole('heading', { name: /grade้r/ }));
+    expect(screen.getByText('“เกรดเด้อ”'));
+    expect(screen.getByText('คลาสเรียนของฉัน'));
+    expect(screen.getByText('เพลย์กราวด์'));
+  });
+});
