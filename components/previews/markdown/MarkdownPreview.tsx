9ca84aa70c5deeea14dde6055e40b4ce41072be8
ercusz <57826174+ercusz@users.noreply.github.com>
2023-01-23 19:12:55 +0700

    feat: :sparkles: render mermaid in MarkdownPreview

--- components/previews/markdown/MarkdownPreview.tsx
+++ components/previews/markdown/MarkdownPreview.tsx
@@ -1,6 +1,8 @@
 import { MarkdownPreviewProps } from '@uiw/react-markdown-preview';
 import '@uiw/react-markdown-preview/markdown.css';
+import mermaid from 'mermaid';
 import dynamic from 'next/dynamic';
+import { useEffect, useRef } from 'react';
 import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
 import remarkToc from 'remark-toc';
 
@@ -13,11 +15,67 @@ export interface IMarkdownPreview {
   content?: string;
 }
 
-const MarkdownPreview: React.FC<IMarkdownPreview> = ({ content }) => {
+const randomId = () => parseInt(String(Math.random() * 1e15), 10).toString(36);
 
+const getCode = (arr = []): string =>
+  arr
+    .map((dt: any) => {
+      if (typeof dt === 'string') {
+        return dt;
+      }
+      if (dt.props && dt.props.children) {
+        return getCode(dt.props.children);
+      }
+      return false;
+    })
+    .filter(Boolean)
+    .join('');
+
+const Code = ({ children = [], className }: any) => {
+  const demoId = useRef(`demo-${randomId()}`);
+  const code = getCode(children);
+  const demo = useRef(null);
+  useEffect(() => {
+    if (demo.current) {
+      try {
+        mermaid.initialize({
+          theme: 'forest',
+        });
+
+        const str = mermaid.render(
+          demoId.current,
+          code,
+          () => null,
+          demo.current
+        );
+        // @ts-ignore
+        demo.current.innerHTML = str;
+      } catch (error) {
+        // @ts-ignore
+        demo.current.innerHTML = error;
+      }
+    }
+  }, [code, demo]);
+
+  if (
+    typeof code === 'string' &&
+    typeof className === 'string' &&
+    /^language-mermaid/.test(className.toLocaleLowerCase())
+  ) {
+    return (
+      <code ref={demo}>
+        <code id={demoId.current} style={{ display: 'none' }} />
+      </code>
+    );
+  }
+  return <code className={String(className)}>{children}</code>;
+};
+
+const MarkdownPreview: React.FC<IMarkdownPreview> = ({ content }) => {
   return (
     <Preview
       source={content}
+      components={{ code: Code }}
       remarkPlugins={[remarkToc]}
       pluginsFilter={(type, plugin) => {
         if (type === 'rehype') {
@@ -55,6 +113,7 @@ const MarkdownPreview: React.FC<IMarkdownPreview> = ({ content }) => {
                     'language-xml',
                     'language-yaml',
                     'language-yml',
+                    'language-mermaid',
                   ],
                 ],
               },
