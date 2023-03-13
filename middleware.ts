59fd7bb3774d5a8273ce7441923da4e73cfed45e
ercusz <57826174+ercusz@users.noreply.github.com>
2023-03-14 00:11:22 +0700

    feat: :sparkles: add protected pages in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,11 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/course/:path*', '/classroom/:path*', '/playground'],
+  matcher: [
+    '/course/:path*',
+    '/classroom/:path*',
+    '/profiles/:path*',
+    '/settings/:path*',
+    '/playground',
+  ],
 };
